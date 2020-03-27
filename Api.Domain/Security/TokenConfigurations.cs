557aabb2bc0e3853c6c12adab880b646b829cb3a
josias.soares <josias.mota.soares@gmail.com>
2020-03-27 10:18:35 -0300

    Ajustes no AppSettings

---
+++ Api.Domain/Security/TokenConfigurations.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Security
+{
+    public class TokenConfigurations
+    {
+        public string Audience { get; set; }
+        public int Issuer { get; set; }
+        public int Seconds { get; set; }
+    }
+}
