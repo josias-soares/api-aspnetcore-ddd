46d077574e1584591eb788a3a4fd671d5cda65b3
josias.soares <josias.mota.soares@gmail.com>
2020-07-24 09:04:09 -0300

    Testes na UserController DELETE

---
+++ Api.Application.Test/Usuario/QuandoRequisitarDelete/RetornoDeleteBadRequest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Application.Controllers;
+using Domain.DTOs.User;
+using Domain.Interfaces.Services.Users;
+using Faker;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace Api.Application.Test.Usuario.QuandoRequisitarDelete
+{
+    public class RetornoUpdateBadRequest
+    {
+        private UsersController _controller;
+
+        [Fact(DisplayName = "É possivel realizar o Delete na UsersController e retornar um BadRequest")]
+        public async Task E_Possivel_Invocar_a_UsersController_Delete_Retornando_BadRequest()
+        {
+            var serviceMock = new Mock<IUserService>();
+
+            serviceMock.Setup(m => m.Delete(It.IsAny<Guid>())).ReturnsAsync(false);
+            
+            _controller = new UsersController(serviceMock.Object);
+            _controller.ModelState.AddModelError("Id", "Formato Invalido");
+            
+            var result = await _controller.Delete(default(Guid));
+            Assert.True(result is BadRequestObjectResult);
+            Assert.False(_controller.ModelState.IsValid);
+        }
+        
+    }
+}
---
+++ Api.Application.Test/Usuario/QuandoRequisitarDelete/RetornoDeleteOK.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Application.Controllers;
+using Domain.DTOs.User;
+using Domain.Interfaces.Services.Users;
+using Faker;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace Api.Application.Test.Usuario.QuandoRequisitarDelete
+{
+    public class RetornoUpdateOk
+    {
+        private UsersController _controller;
+
+        [Fact(DisplayName = "É possivel realizar o Delete na UsersControllere retornar true")]
+        public async Task E_Possivel_Invocar_a_UsersController_Delete_Retornando_True()
+        {
+            var serviceMock = new Mock<IUserService>();
+
+            serviceMock.Setup(m => m.Delete(It.IsAny<Guid>())).ReturnsAsync(true);
+            
+            _controller = new UsersController(serviceMock.Object);
+       
+            var result = await _controller.Delete(Guid.NewGuid());
+            
+            Assert.True(result is OkObjectResult);
+
+            var resultValue = ((OkObjectResult) result).Value;
+            Assert.NotNull(resultValue);
+            Assert.True((bool) resultValue);
+        }
+        
+    }
+}
